2c36343b075cef8dd413542a680880c643dc8188
Atharva Patil <patil8atharva@gmail.com>
2024-09-17 10:27:37 +0530

    basic manage cart component

--- api/cart.ts
+++ api/cart.ts
@@ -1,21 +1,21 @@
-import { Cart, PartnerID, PartnerSessionID, Vegetable, CatalogData } from "@/types/types";
+import { Item, Cart, PartnerID, PartnerSessionID, Vegetable, CatalogData } from "@/types/types";
 const baseUrl = process.env.EXPO_PUBLIC_API_URL;
 
 export async function getCurrentPartnerSession(): Promise<PartnerSessionID> {
     if (__DEV__) {
-        return new Promise(() => process.env.EXPO_PARTNER_SESSION_ID as PartnerSessionID);
+        return Promise.resolve(process.env.EXPO_PUBLIC_PARTNER_SESSION_ID as PartnerSessionID);
     } else {
         //TODO: fetch the PartnerSessionID from Async
-        return new Promise(() => "" as PartnerSessionID);
+        return Promise.resolve("" as PartnerSessionID);
     }
 }
 
 export async function getCurrentPartnerID(): Promise<PartnerID> {
     if (__DEV__) {
-        return new Promise(() => process.env.EXPO_PARTNER_ID)
+        return Promise.resolve(parseInt(process.env.EXPO_PUBLIC_PARTNER_ID ?? "1") as PartnerID);
     } else {
         //TODO: fetch the PartnerSessionID from Async
-        return new Promise(() => 0 as PartnerID);
+        return Promise.resolve(0 as PartnerID);
     }
 }
 
@@ -32,7 +32,11 @@ export async function addVegetableToCart(vegetable: Vegetable) {
     //TODO: PUT the vegetable to partner's cart using the /cart/<partner_id> endpoint
     const partnerID = await getCurrentPartnerID();
     const sessionID = await getCurrentPartnerSession();
-    const response = await fetch(`${baseUrl}/cart/${partnerID}`, { method: 'PUT', headers: new Headers({ "SessionID": sessionID }) });
+    const item = { vegetableId: vegetable.id, price: 0.0 } as Item;
+    const response = await fetch(`${baseUrl}/cart/${partnerID}`, { method: 'PUT', 
+        headers: { "SessionID": sessionID, "Content-Type": "application/json", }, 
+        body: JSON.stringify(item) });
+
     if (response.status != 200) {
         console.log("error adding vegetable to cart");
     }
@@ -40,6 +44,8 @@ export async function addVegetableToCart(vegetable: Vegetable) {
 
 export async function removeVegetableFromCart(vegetable: Vegetable) {
     //TODO: DELETE the vegetable to partner's cart using the /cart/<partner_id> endpoint
+    const partnerID = await getCurrentPartnerID();
+    const response = await fetch(`${baseUrl}/cart/${partnerID}`);
 }
 
 export async function getPartnerCart(): Promise<Cart> {
@@ -50,6 +56,6 @@ export async function getPartnerCart(): Promise<Cart> {
         console.log("error fetching partner's cart");
     }
     const cart_json = await response.json();
-    const cart = cart_json.cart as Cart;
+    const cart = cart_json.items as Cart;
     return cart;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,31 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { CatalogSkeleton, Catalog } from "@/components/Catalog";
-import { Vegetable, CatalogData} from "@/types/types";
+import { Vegetable, CatalogData } from "@/types/types";
 import { getVegetableCatalog, addVegetableToCart, removeVegetableFromCart } from "@/api/cart";
+import { CatalogContext } from "./_layout";
+import { router } from "expo-router";
 
 export default function Index() {
   const [cart, setCart] = useState<Set<Vegetable>>(new Set());
 
-  function handleVeggie(vegetable: Vegetable) {
+  async function handleVeggie(vegetable: Vegetable) {
     setCart(new Set(cart.add(vegetable)));
-    addVegetableToCart(vegetable);
+    await addVegetableToCart(vegetable);
   }
 
-  let [catalog, setCatalog] = useState<CatalogData>(new Map() as CatalogData);
+  //let [catalog, setCatalog] = useState<CatalogData>(new Map() as CatalogData);
   let [catalogLoading, setCatalogLoading] = useState<boolean>(true);
   let [catalogError, setCatalogError] = useState<boolean>(false);
+  const { catalog, setCatalog } = useContext(CatalogContext);
   useEffect(() => {
-    getVegetableCatalog().then((data) => {
-      //TODO: cache catalog data in the AsyncStorage for a period of time i.e associcate a TTL with CatalogData
-      
-      //currently simulating time taken to fetch catalog data
+    if (Object.keys(catalog).length === 0) {
+      getVegetableCatalog().then((data) => {
+        //TODO: cache catalog data in the AsyncStorage for a period of time i.e associcate a TTL with CatalogData
         setCatalog(data);
         setCatalogLoading(false);
-
-    }).catch(() => setCatalogError(true));
+      }).catch(() => setCatalogError(true));
+    }
   }, []);
   console.log(cart);
   if (catalogError) { return (<View><Text>Error</Text></View>); }
@@ -37,7 +39,7 @@ export default function Index() {
 
       {/*only show the "Manage Cart" button if the cart isn't empty*/}
       {cart.size > 0 &&
-        <TouchableOpacity style={styles.menuBar}>
+        <TouchableOpacity style={styles.menuBar} onPress={()=>router.push('/cart')}>
           <Text style={{ color: "white" }}>Manage Cart</Text>
         </TouchableOpacity>}
     </View>
